19f8884939ef4370fe92e1378f1d275ed4d3e32d
alriyadsouza <alriyatreeza929@gmail.com>
2023-05-25 23:55:56 +0530

    made changes

--- lib/main.dart
+++ lib/main.dart
@@ -19,69 +19,84 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.red,// foreground
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.red,// foreground
+                  ),
+                  onPressed: () {
+                    soundButton(1);
+                  },
+                  child: Text(''),
                 ),
-                onPressed: () {
-                  soundButton(1);
-                },
-                child: Text('Click me1'),
               ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.orange,// foreground
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.orange,// foreground
+                  ),
+                  onPressed: () {
+                    soundButton(2);
+                  }, child: Text(''),
                 ),
-                onPressed: () {
-                  soundButton(2);
-                },
-                child: Text('Click me2'),
               ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.yellow,// foreground
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.yellow,// foreground
+                  ),
+                  onPressed: () {
+                    soundButton(3);
+                  },
+                  child: Text(''),
                 ),
-                onPressed: () {
-                  soundButton(3);
-                },
-                child: Text('Click me3'),
               ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.green,// foreground
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,// foreground
+                  ),
+                  onPressed: () {
+                    soundButton(4);
+                  },
+                  child: Text(''),
                 ),
-                onPressed: () {
-                  soundButton(4);
-                },
-                child: Text('Click me4'),
               ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.purple,// foreground
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.purple,// foreground
+                  ),
+                  onPressed: () {
+                    soundButton(5);
+                  },
+                  child: Text(''),
                 ),
-                onPressed: () {
-                  soundButton(5);
-                },
-                child: Text('Click me5'),
               ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.teal,// foreground
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.teal,// foreground
+                  ),
+                  onPressed: () {
+                    soundButton(6);
+                  },
+                  child: Text(''),
                 ),
-                onPressed: () {
-                  soundButton(6);
-                },
-                child: Text('Click me6'),
               ),
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue,// foreground
+              Expanded(
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,// foreground
+                  ),
+                  onPressed: () {
+                    soundButton(7);
+                  },
+                  child: Text(''),
                 ),
-                onPressed: () {
-                  soundButton(7);
-                },
-                child: Text('Click me7'),
               ),
             ],
           ),
